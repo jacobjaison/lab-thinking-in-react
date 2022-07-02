31d8bdb09c41584156e7571a4140b94b075cd05c
Jaison Jacob <jacob.jaison@hotmail.com>
2022-07-02 17:19:28 +0400

    Updated Changes

--- src/components/SearchInstock.jsx
+++ src/components/SearchInstock.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 
-const SearchInstock = ({searchedInstock, setSearchedInstock}) => {
+const SearchInstock = ({searchedInStock, setSearchedInStock}) => {
     
   return <div>
   <br />
-    <input type="checkbox" className ='searchedInstock' id='searchedInstock' onChange= {(e) => setSearchedInstock(e.target.checked)}
+    <input type="checkbox" className ='searchedInStock' id='searchedInStock' onChange= {(e) => setSearchedInStock(e.target.checked)}
     />
-    <label forHTML='searchedInstock'> Only Show Products in stock  </label>
+    <label forHTML='searchedInStock'> Only Show Products in stock  </label>
 
     <hr />
   </div>
